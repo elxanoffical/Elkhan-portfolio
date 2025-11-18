6c6bca0770a4c878e859f757ababf41d3bfe3af4
Elxan <elxan.offical.07@gmail.com>
2025-11-18 20:25:04 +0400

    first start

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center bg-gradient-to-br from-gray-900 via-gray-800 to-black">
+      <h1 className="text-6xl font-bold text-amber-400 drop-shadow-[0_0_25px_#fbbf24] animate-pulse">
+        Welcome to my Elkhan's portfolio
+      </h1>
+    </div>
+  );
+}
